3587a4bb5671e07ffd68dc783e4e640974fb3cc1
estherKANGBIETE <52072473+estherKANGBIETE@users.noreply.github.com>
2020-07-14 16:57:33 +0000

    docker configurations

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3
+ENV PYTHONUNBUFFERED 1
+RUN mkdir /code
+WORKDIR /code
+COPY requirements.txt /code/
+RUN pip install -r requirements.txt
+RUN groupadd -g 1000 rwanda
+RUN useradd -u 1000 -ms /bin/bash  -g rwanda rwanda
+USER rwanda
--- rwanda/settings.py
+++ rwanda/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'rwanda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgre',
+        'PORT': 5432,
+
     }
 }
 
