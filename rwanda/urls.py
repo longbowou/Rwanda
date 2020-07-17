6eefe76a9defd37934a1e1980466e0d0cecc3d60
estherKANGBIETE <52072473+estherKANGBIETE@users.noreply.github.com>
2020-07-17 18:40:34 +0000

    [update] settings, urls(rwanda) [createV grapheql package

--- rwanda/urls.py
+++ rwanda/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
+
+from rwanda.grapheql.shema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path("graphql", GraphQLView.as_view(graphiql=True)),
+    path("graphql", csrf_exempt(GraphQLView.as_view(schema=schema, graphiql=True))),
 ]
