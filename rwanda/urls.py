923f87629444985b68fb3b5f740af0ec340ef1e1
Longbowou <blandedaniel@gmail.com>
2020-08-06 00:59:34 +0000

    [update] readme !

--- rwanda/urls.py
+++ rwanda/urls.py
@@ -1,18 +1,3 @@
-"""rwanda URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -27,7 +12,7 @@ from rwanda.graphql.shema import schema
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("graphql/", csrf_exempt(GraphQLView.as_view(schema=schema, graphiql=True))),
-    path("admin-graphql/", csrf_exempt(GraphQLView.as_view(schema=admin_schema, graphiql=True))),
+    path("graphql-admin/", csrf_exempt(GraphQLView.as_view(schema=admin_schema, graphiql=True))),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
