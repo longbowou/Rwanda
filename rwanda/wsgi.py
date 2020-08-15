a97269f492c31b40a58c00dbde1b044c69077fd3
Longbowou <blandedaniel@gmail.com>
2020-08-15 23:58:26 +0000

    [wsgi] !

--- rwanda/wsgi.py
+++ rwanda/wsgi.py
@@ -8,24 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-import signal
-import sys
-import time
-import traceback
 
-from django.core.wsgi import WSGIHandler
-
-sys.path.append('/var/www/clients/client1/web16/web/backend')
-
-sys.path.append('/var/www/clients/client1/web16/web/venv/lib/python3.8/site-packages')
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rwanda.settings')
 
-try:
-    application = WSGIHandler()
-except Exception:
-    # Error loading applications
-    if 'mod_wsgi' in sys.modules:
-        traceback.print_exc()
-        os.kill(os.getpid(), signal.SIGINT)
-        time.sleep(2.5)
+application = get_wsgi_application()
