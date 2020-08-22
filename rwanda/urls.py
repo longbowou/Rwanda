02e8a0f6a9635440bbd0562260b13b4d154cbc27
Longbowou <blandedaniel@gmail.com>
2020-08-22 00:09:28 +0000

    [update] graphql configs !

--- rwanda/urls.py
+++ rwanda/urls.py
@@ -5,7 +5,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
-from rest_framework_jwt.views import obtain_jwt_token
 
 from rwanda.graphql.admin_shema import admin_schema
 from rwanda.graphql.shema import schema
@@ -14,7 +13,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("graphql/", csrf_exempt(GraphQLView.as_view(schema=schema, graphiql=True))),
     path("graphql-admin/", csrf_exempt(GraphQLView.as_view(schema=admin_schema, graphiql=True))),
-    path("api-token-auth/", obtain_jwt_token),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
