823a174041c0ee099ef6a69c8968733fabf309e2
Longbowou <blandedaniel@gmail.com>
2020-09-03 01:07:16 +0000

    [update] setting cors !

--- rwanda/settings.py
+++ rwanda/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'http://backend-rwanda.cifs-togo.com',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
