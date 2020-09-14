5cdda693e08768a028ea2a9a8e9ca7f819a9e61d
Longbowou <blandedaniel@gmail.com>
2020-09-14 06:34:18 +0000

    [update] account queries !

--- rwanda/graphql/schemas/account.py
+++ rwanda/graphql/schemas/account.py
@@ -6,21 +6,28 @@ from rwanda.administration.models import Parameter
 from rwanda.graphql.account.mutations import AccountMutations
 from rwanda.graphql.account.queries import AccountQueries
 from rwanda.graphql.admin.mutations import AdminMutations
+from rwanda.graphql.decorators import account_required
 from rwanda.graphql.purchase.mutations import PurchaseMutations
 from rwanda.graphql.service.mutations import ServiceMutations
 from rwanda.graphql.types import ServiceCategoryType, ServiceType, LitigationType, ServicePurchaseType, ParametersType
+from rwanda.service.models import Service
 
 
 class AccountQuery(AccountQueries):
     service_categories = DjangoFilterListField(ServiceCategoryType)
     service_purchases = DjangoFilterListField(ServicePurchaseType)
     services = DjangoFilterListField(ServiceType)
+    service = graphene.Field(ServiceType, id=graphene.UUID(required=True))
     litigation = DjangoFilterListField(LitigationType)
     parameters = graphene.Field(ParametersType)
 
     def resolve_parameters(root, info, **kwargs):
         return ParametersType(currency=Parameter.objects.get(label=Parameter.CURRENCY).value,
                               base_price=Parameter.objects.get(label=Parameter.BASE_PRICE).value)
+
+    @account_required
+    def resolve_service(self, info, id):
+        return Service.objects.filter(pk=id).first()
 
 
 class AccountMutation(AccountMutations, ServiceMutations, PurchaseMutations, AdminMutations):
