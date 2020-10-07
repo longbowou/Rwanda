892774bf7061442c3812a8239f0cb5247c16ae49
Longbowou <blandedaniel@gmail.com>
2020-10-07 20:56:37 +0000

    [update] uvicorn config !

--- rwanda/routing.py
+++ rwanda/routing.py
@@ -1,9 +1,13 @@
+import os
+
 import channels
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter
 from django.urls import path
 
 from rwanda.graphql.consumers import AccountGraphqlWsConsumer
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rwanda.settings')
 
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
