d915c209ffc38a219c764738bc6addd7e9a08a07
Longbowou <blandedaniel@gmail.com>
2020-10-07 09:04:53 +0000

    [update] circus config !

--- rwanda/asgi.py
+++ rwanda/asgi.py
@@ -8,18 +8,13 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import channels
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter
 from django.urls import path
 
 from rwanda.graphql.consumers import AccountGraphqlWsConsumer
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rwanda.settings')
-
 application = ProtocolTypeRouter({
-    'websocket': AuthMiddlewareStack(
-        channels.routing.URLRouter([
-            path('graphql-ws/', AccountGraphqlWsConsumer),
-        ])
-    )
+    'websocket': channels.routing.URLRouter([
+        path('graphql-ws/', AccountGraphqlWsConsumer),
+    ])
 })
