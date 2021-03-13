2a1e84e5d30961107ff1803d9d373b3ce904b195
Longbowou <blandedaniel@gmail.com>
2021-03-13 10:18:01 +0000

    [refactor] configs !

--- rwanda/celery.py
+++ rwanda/celery.py
@@ -5,12 +5,10 @@ import os
 from celery import Celery
 # set the default Django settings module for the 'celery' program.
 from celery.schedules import crontab
-from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rwanda.settings')
 
-app = Celery('rwanda', broker=settings.CELERY_BROKER_URL)
-
+app = Celery('rwanda')
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
