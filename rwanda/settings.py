ff1b2a264f61c56743fbfd15cd734c83c3e78500
Longbowou <blandedaniel@gmail.com>
2021-11-09 18:00:03 +0000

    [update] refactors !

--- rwanda/settings.py
+++ rwanda/settings.py
@@ -272,7 +272,7 @@ DEFAULT_FROM_EMAIL = 'blandedaniel@gmail.com'
 
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_CACHE_BACKEND = 'django-cache'
-CELERY_BROKER_URL = 'redis://rwanda_redis:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
